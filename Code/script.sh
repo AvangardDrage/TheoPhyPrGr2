d13cfaca9876f80e407ec33ea16b185039b2106f
davideturco <46572747+davideturco@users.noreply.github.com>
2019-02-19 14:12:15 +0000

    Updated bash script

--- Code/script.sh
+++ Code/script.sh
@@ -24,16 +24,6 @@ gnuplot <<- EOF
   unset clabel
   unset key
 
-  #set palette defined ( 0 "#000090",\
-                        1 "#000fff",\
-                        2 "#0090ff",\
-                        3 "#0fffee",\
-                        4 "#90ff70",\
-                        5 "#ffee00",\
-                        6 "#ff7000",\
-                        7 "#ee0000",\
-                        8 "#7f0000")
-
 
   set palette defined ( 0 "#1212B0",\
                         1 "#242FE6",\
@@ -44,45 +34,53 @@ gnuplot <<- EOF
                         6 "#ff7000",\
                         7 "#E11313",\
                         8 "#950101")
+
+
   #vector scale
-  h = 0.1
+  h = 50
 
 
   set title "Electric potential (numerical)"
-  plot [0:${rows}-1] [0:${cols}-1] "test_solution.dat" using 1:2:3 with image
+  plot [0:${rows}-1] [0:${cols}-1] "n_SOR.dat" using 1:2:3 with image
 
   set title "Electric potential (analytical)"
-  plot [0:${rows}-1] [0:${cols}-1] "solution_new.dat" using 1:2:3 with image
-
-  set title "Errors"
-  plot [0:${rows}-1] [0:${cols}-1] "test_solution.dat" using 1:2:6 with image
-
-  set title ""
-  set dgrid3d 100,100,2
-  set view 10,30,1.2,5
-
-  #set pm3d lighting primary 0.3 specular 0.6
-  #splot [-4.95:4.95] [-4.95:4.95] "solution.dat" using 1:2:6 with pm3d
+  plot [0:${rows}-1] [0:${cols}-1] "a_sol_2.dat" using 1:2:3 with image
 
   set title "Electric field"
-  plot [0:${rows}-1] [0:${cols}-1] "test_solution.dat" using 1:2:4 with image
+  plot [0:${rows}-1] [0:${cols}-1] "n_SOR.dat" using 1:2:4 with image
 
   set contour base
   unset surface
   set dgrid3d 10,10,4
   set cntrparam bspline
-  set cntrparam levels incremental -8, 2, 8
+  set cntrparam levels incremental -4, 0.1, 4
   set table "cont.dat"
-  splot "test_solution.dat" u 1:2:3
+  splot [0:${rows}-1] [0:${cols}-1] "n_SOR.dat" u 1:2:3
   unset table
 
 
   set title "Electric field"
   set linestyle 1 lt 2 lc rgb "#77000000"
-  plot [0:${rows}-1] [0:${cols}-1] "test_solution.dat" using 1:2:4 with image, \
-  	"quiver.dat" u ($1 - h*0.5*$5):($2 - h*0.5*$6):(h*$5):(h*$6)\
+  plot [0:${rows}-1] [0:${cols}-1] "n_SOR.dat" using 1:2:4 with image, \
+  	[0:${rows}-1] [0:${cols}-1] "n_SOR_quiver.dat" u ($1 - h*0.5*$3):($2 - h*0.5*$4):(h*$3):(h*$4)\
   	with vectors head size 0.04,40,20 filled lc 'black', \
   	"cont.dat" u 1:2:3 with lines ls 1
+
+
+
+  set title "Electric potential (analytical)"
+  plot [0:${rows}-1] [0:${cols}-1] "a_sol_2.dat" using 1:2:3 with image
+
+  set title "Errors"
+  plot [0:${rows}-1] [0:${cols}-1] "a_sol_2_err.dat" using 1:2:3 with image
+
+
+  set title ""
+  set dgrid3d 100,100,2
+  set view 10,30,1.2,5
+
+  set pm3d lighting primary 0.3 specular 0.6
+  splot [0:${rows}-1] [0:${cols}-1] "a_sol_2_err.dat" using 1:2:3 with pm3d
 
 EOF
 
