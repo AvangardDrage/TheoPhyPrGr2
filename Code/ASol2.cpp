6969e2ecfa9e79b0e895446b93c09fbcfb7b11e6
f78bono <46572830+f78bono@users.noreply.github.com>
2019-02-20 00:21:04 +0000

    ASol only works with atan and multiplying by 2

--- Code/ASol2.cpp
+++ Code/ASol2.cpp
@@ -9,13 +9,12 @@ void ASol2(Eigen::Matrix <double, Eigen::Dynamic, Eigen::Dynamic>& asol, Circle&
 
 
   double r;
-  double theta;			  //independent variables in cylindical 
-	
+  double theta;			  //independent variables in cylindical
+
   std::ofstream outfile;
   outfile.open("a_sol_2.dat");
 
-
-  int bp_s=clock();
+ // int bp_s=clock();
   //Calculate the analytical solution
   for (int i = 0; i < asol.rows(); i++)
     {
@@ -23,64 +22,46 @@ void ASol2(Eigen::Matrix <double, Eigen::Dynamic, Eigen::Dynamic>& asol, Circle&
       for (int j = 0; j < asol.cols(); j++)
 	{
 
-	  double rx = static_cast<double>(cobj.centre[1]-j);
-	  double ry = static_cast<double>(cobj.centre[0]-i);
+	  double rx = static_cast<double>(j - cobj.centre[1]);
+	  double ry = static_cast<double>(i - cobj.centre[0]);
 
 	    // std::cout << "--------------------------------" << std::endl;
 	    // std::cout << "i: " << i << "    " << "j: " << j << std::endl;
 	    // std::cout << "rx: "<< rx << "    " << "ry: "<< ry << std::endl;
 
-	  if(j!=cobj.centre[1])
-	    {
-
 	      // convert cartesian into 2d cylindical coordinate
-	      r = sqrt(pow(rx,2)+pow(ry,2));
-	      theta = atan(ry/rx);
+	   r = sqrt(pow(rx,2)+pow(ry,2));
+	   theta = atan(ry/rx);
 
-	      // std::cout << "Gate 1: r =  " << r << "; theta = " << theta << std::endl;
-	      // calculate the solution
 
-	      if(j <= pplate.centre[0])
-		{
-		  //  std::cout <<"Gate 2.1" << std::endl;
-		  asol(i,j) = pplate.potential;
-		}
-	      else if(j>=nplate.centre[0])
-		{
-		  // std::cout <<"Gate 2.2" << std::endl;
-		  asol(i,j) = nplate.potential;
-		}
 
-	      // if r < a then the potnetial is zero as it is insinde the grounded cylinder
-	      else if (r <= cobj.radius)
-		{
-		  // std::cout <<"Gate 2.3" << std::endl;
-		  asol(i,j)=0;
-		}
+	   // if r < a then the potnetial is zero as it is insinde the grounded cylinder
+	   if (r <= cobj.radius)
+	      {
+		 asol(i,j)=0;
+	      }
 	      //else calculate the solution between r=a to r=d/cos(theta) from the analytical solution
 	      // on the negative x-axis, potential is set to +V, on the positive it is -V
-	      else if (j < cobj.centre[0])
+	   else if (j < cobj.centre[1] && j > pplate.centre[0])
 		{
 		  // std::cout << "Gate 2.4" << std::endl;
-		  asol(i,j) = cos(theta)*(r-pow(cobj.radius,2)/r)*(pplate.potential*(nplate.centre[0]-pplate.centre[0]))/(pow((nplate.centre[0]-pplate.centre[0]),2)-pow(cos(theta),2)*pow(cobj.radius,2));
+		 asol(i,j) = 2*cos(theta)*(r-pow(cobj.radius,2)/r)*(pplate.potential*(nplate.centre[0]-pplate.centre[0]))/(pow((nplate.centre[0]-pplate.centre[0]),2)-pow(cos(theta),2)*pow(cobj.radius,2));
 		}
-	      else if (j > cobj.centre[0])
+	   else if (j > cobj.centre[1] && j < nplate.centre[0])
 		{
 		  // std::cout << "Gate 2.5" << std::endl;
-		  asol(i,j) = cos(theta)*(r-pow(cobj.radius,2)/r)*(nplate.potential*(nplate.centre[0]-pplate.centre[0]))/(pow((nplate.centre[0]-pplate.centre[0]),2)-pow(cos(theta),2)*pow(cobj.radius,2));
+		 asol(i,j) = 2*cos(theta)*(r-pow(cobj.radius,2)/r)*(nplate.potential*(nplate.centre[0]-pplate.centre[0]))/(pow((nplate.centre[0]-pplate.centre[0]),2)-pow(cos(theta),2)*pow(cobj.radius,2));
 		}
+
 	      // std::cout << "Gate 3" << std::endl;
 	      outfile << j << " " << i << " " << asol(i,j) << std:: endl;
 	      // std::cout << "--------------------------------" << std::endl;
-	    }
+
 	  //  std::cout << "*********************************" << std::endl;
 	}
     }
-  int ep_s=clock();
-  std::cout<<"Analytical Method time: " << ( ep_s-bp_s )/double(CLOCKS_PER_SEC)<<" seconds" <<std::endl;
+  //int ep_s=clock();
+ // std::cout<<"Analytical Method time: " << ( ep_s-bp_s )/double(CLOCKS_PER_SEC)<<" seconds" <<std::endl;
 
   outfile.close();
 }
-
-
-
